34777e4d0e375b1b8d216ff1958102f1b0abec28
Zach Vilardell <szvila01@louisville.edu>
2018-01-04 14:33:52 -0500

    find most common palindrome

--- PalindromeCounting.playground/Contents.swift
+++ PalindromeCounting.playground/Contents.swift
@@ -7,7 +7,8 @@ import UIKit
 
 let sentence = "kayak anna racecar zach kayak mom palindrome dad falcon kayak hannah bob anna dog"
 
-func countPalindromes(sentence: String) -> [String:Int] {
+//return a dictionary containing count of each palindrome found in a given sentence
+func countPalindromes(in sentence: String) -> [String:Int] {
     var palindromeCountByWord: [String:Int] = [:]
     
     let slicedWords = sentence.components(separatedBy: " ")
@@ -29,4 +30,26 @@ func countPalindromes(sentence: String) -> [String:Int] {
     return palindromeCountByWord
 }
 
-print(countPalindromes(sentence: sentence))
+print(countPalindromes(in: sentence))
+
+//-----------------------------------------------------------------------------
+
+//return the most common palindrome in a given sentence
+func mostCommonPalindrome(in sentence: String) -> String? {
+    let palindromeCountByWord = countPalindromes(in: sentence)
+    
+    var mostCommonPalindrome: String? = nil
+    var maxCount = 0
+    
+    for (palindrome, count) in palindromeCountByWord {
+        if count > maxCount {
+            //new candidate found
+            mostCommonPalindrome = palindrome
+            maxCount = count
+        }
+    }
+    
+    return mostCommonPalindrome
+}
+
+print("Most common palindrome: \(mostCommonPalindrome(in: sentence) ?? "n/a")")
