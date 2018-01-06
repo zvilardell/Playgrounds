57e8e8dfcb5b470fab9a9abad96cf8fef70486a6
Zach Vilardell <szvila01@louisville.edu>
2018-01-06 15:23:31 -0500

    modified fibonacci functions

--- Fibonacci.playground/Contents.swift
+++ Fibonacci.playground/Contents.swift
@@ -3,45 +3,53 @@
 import UIKit
 
 //iterative fibonacci sequence generator up to passed-in count
-func iterativeFib(count: Int) {
+func iterativeFib(count: Int) -> [Int] {
     //must pass in positive count
     guard count > 0 else {
-        return
+        return []
     }
+    var fibNums = [Int]()
     var prevNum = 0
     var currentNum = 1
     for _ in 1...count {
-        print(currentNum)
+        fibNums.append(currentNum)
         let temp = currentNum
         currentNum = prevNum + currentNum
         prevNum = temp
     }
-}
-
-//memoized recursive fibonacci number calculation
-func recursiveFib(count: Int, fibNum: inout [Int:Int]) -> Int {
-    //must pass in positive count
-    guard count > 0 else {
-        return 0
-    }
-    if count <= 2 {
-        fibNum[count] = 1
-        return fibNum[count] ?? 0
-    }
-    fibNum[count] = (fibNum[count - 2] ?? recursiveFib(count: count - 2, fibNum: &fibNum)) + (fibNum[count - 1] ?? recursiveFib(count: count - 1, fibNum: &fibNum))
-    return fibNum[count] ?? 0
+    return fibNums
 }
 
 //print fibonacci sequence up to 'count' using iterative function
-//iterativeFib(count: 8)
+print(iterativeFib(count: 8))
 
-//print fibonacci sequence up to 'count' using recursive function with memo dictionary
-var fibNum: [Int:Int] = [:]
-recursiveFib(count: 8, fibNum: &fibNum)
-for i in fibNum.keys.sorted()/*1...fibNum.count*/ {
-    print(fibNum[i] ?? 0)
+//--------------------------------------------------------------------------------
+
+func recursiveFib(numSteps: Int) -> [Int] {
+    var fibDict: [Int:Int] = [:]
+    recursiveFib(numSteps: numSteps, memo: &fibDict)
+    var fibNums = [Int]()
+    for i in fibDict.keys.sorted() {
+        fibNums.append(fibDict[i] ?? 0)
+    }
+    return fibNums
 }
 
+func recursiveFib(numSteps: Int, memo: inout [Int:Int]) -> Int {
+    guard numSteps > 0 else {
+        return 0
+    }
+    guard numSteps > 2 else {
+        memo[numSteps] = 1
+        return 1
+    }
+    
+    memo[numSteps] = (memo[numSteps - 1] ?? recursiveFib(numSteps: numSteps - 1, memo: &memo)) +
+                     (memo[numSteps - 2] ?? recursiveFib(numSteps: numSteps - 2, memo: &memo))
+    
+    return memo[numSteps] ?? 0
+}
 
+print(recursiveFib(numSteps: 8))
 
 
